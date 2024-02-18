71882f3163d9654806921d7ace1d7c3f5a6471cd
Adithyan A <deepaadithyan56@gmail.com>
2024-02-18 03:38:45 -0800

    Lvim cofig updated

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -2,4 +2,38 @@
 -- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
 -- Forum: https://www.reddit.com/r/lunarvim/
 -- Discord: https://discord.com/invite/Xb9B4Ny
-lvim.transparent_window=true
+lvim.transparent_window = true
+
+lvim.plugins = {
+  {
+    "Mofiqul/dracula.nvim",
+    config = function()
+      require('lualine').setup {
+        options = {
+          theme = 'dracula-nvim'
+        }
+      }
+    end,
+  },
+  {
+    "navarasu/onedark.nvim",
+    config = function()
+      require('lualine').setup {
+        options = {
+          theme = 'onedark'
+        }
+      }
+    end,
+  },
+
+  {
+    "Mofiqul/vscode.nvim",
+    config = function()
+      require('lualine').setup {
+        options = {
+          theme = 'vscode'
+        }
+      }
+    end,
+  },
+}
