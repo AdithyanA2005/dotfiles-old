35a7855465ccae22a974a11b3b22b8ff99b0bca6
Adithyan A <deepaadithyan56@gmail.com>
2024-07-02 22:34:16 +0530

    New plugins added to lunarvim

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -3,9 +3,16 @@
 -- Forum: https://www.reddit.com/r/lunarvim/
 -- Discord: https://discord.com/invite/Xb9B4Ny
 lvim.transparent_window = true
-lvim.builtin.treesitter.autotag.enable = true
-vim.opt.relativenumber = true
+lvim.builtin.treesitter.rainbow.enable = true
 
+vim.opt.relativenumber = true
+-- use treesitter folding
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+
+lvim.builtin.which_key.mappings["sT"] = {
+  "<cmd>TodoTelescope<cr>", "TODO'S"
+}
 lvim.builtin.which_key.mappings["t"] = {
   name = "Terminal",
   f = { "<cmd>ToggleTerm direction=float<cr>", "Float" },
@@ -25,6 +32,15 @@ lvim.plugins = {
     end,
   },
   {
+    "windwp/nvim-ts-autotag",
+    config = function()
+      require("nvim-ts-autotag").setup()
+    end,
+  },
+  {
+    "mrjones2014/nvim-ts-rainbow",
+  },
+  {
     "navarasu/onedark.nvim",
     config = function()
       require('lualine').setup {
@@ -34,7 +50,29 @@ lvim.plugins = {
       }
     end,
   },
+  {
+    "tpope/vim-surround",
 
+    -- make sure to change the value of `timeoutlen` if it's not triggering correctly, see https://github.com/tpope/vim-surround/issues/117
+    -- setup = function()
+    --  vim.o.timeoutlen = 500
+    -- end
+  },
+  {
+    "iamcco/markdown-preview.nvim",
+    build = "cd app && npm install",
+    ft = "markdown",
+    config = function()
+      vim.g.mkdp_auto_start = 1
+    end,
+  },
+  {
+    "folke/todo-comments.nvim",
+    event = "BufRead",
+    config = function()
+      require("todo-comments").setup()
+    end,
+  },
   {
     "Mofiqul/vscode.nvim",
     config = function()
