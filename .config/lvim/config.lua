15a466a0426be13774ae9d9c5f9e7418c18fbcf4
Adithyan A <deepaadithyan56@gmail.com>
2024-12-27 07:52:52 +0530

    Change theme and manage plugins for lunarvim

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -2,17 +2,20 @@
 -- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
 -- Forum: https://www.reddit.com/r/lunarvim/
 -- Discord: https://discord.com/invite/Xb9B4Ny
+
+lvim.colorscheme = "dracula"
 lvim.transparent_window = true
 lvim.builtin.treesitter.rainbow.enable = true
 
 vim.opt.relativenumber = true
--- use treesitter folding
 vim.opt.foldmethod = "expr"
 vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+vim.opt.foldlevelstart = 10 -- folds upto level 10 will be open
 
 lvim.builtin.which_key.mappings["sT"] = {
-  "<cmd>TodoTelescope<cr>", "TODO'S"
+  "<cmd>TodoTelescope<cr>", "Todo's"
 }
+
 lvim.builtin.which_key.mappings["t"] = {
   name = "Terminal",
   f = { "<cmd>ToggleTerm direction=float<cr>", "Float" },
@@ -21,66 +24,29 @@ lvim.builtin.which_key.mappings["t"] = {
 }
 
 lvim.plugins = {
+  { "navarasu/onedark.nvim" },
+  { "github/copilot.vim" },
+  { "tpope/vim-surround" },
+  { "mrjones2014/nvim-ts-rainbow" },
   {
-    "Mofiqul/dracula.nvim",
-    config = function()
-      require('lualine').setup {
-        options = {
-          theme = 'dracula-nvim'
-        }
-      }
-    end,
+    "binhtran432k/dracula.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {}
   },
   {
     "windwp/nvim-ts-autotag",
-    config = function()
-      require("nvim-ts-autotag").setup()
-    end,
-  },
-  {
-    "mrjones2014/nvim-ts-rainbow",
-  },
-  {
-    "navarasu/onedark.nvim",
-    config = function()
-      require('lualine').setup {
-        options = {
-          theme = 'onedark'
-        }
-      }
-    end,
-  },
-  {
-    "tpope/vim-surround",
-
-    -- make sure to change the value of `timeoutlen` if it's not triggering correctly, see https://github.com/tpope/vim-surround/issues/117
-    -- setup = function()
-    --  vim.o.timeoutlen = 500
-    -- end
+    config = function() require("nvim-ts-autotag").setup({ opts = { enable_close_on_slash = true } }) end
   },
   {
     "iamcco/markdown-preview.nvim",
     build = "cd app && npm install",
     ft = "markdown",
-    config = function()
-      vim.g.mkdp_auto_start = 1
-    end,
+    config = function() vim.g.mkdp_auto_start = 1 end
   },
   {
     "folke/todo-comments.nvim",
     event = "BufRead",
-    config = function()
-      require("todo-comments").setup()
-    end,
-  },
-  {
-    "Mofiqul/vscode.nvim",
-    config = function()
-      require('lualine').setup {
-        options = {
-          theme = 'vscode'
-        }
-      }
-    end,
+    config = function() require("todo-comments").setup() end
   },
 }
